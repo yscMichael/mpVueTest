cabc900017ecebf23e953856c3be840abc559921
杨世川 <yscbbc@gmail.com>
2019-07-30 10:35:13 +0800

    代码优化

--- my-project/src/main.js
+++ my-project/src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App'
 
+<!-- 关闭Vue的生产提示消息 -->
 Vue.config.productionTip = false
+<!-- 生命当前组件的类型 -->
 App.mpType = 'app'
 
+<!-- 中间可以暴漏全局组件接口!!!! -->
+
+<!-- 生成应用实例 -->
 const app = new Vue(App)
+<!-- 挂载整个应用 -->
 app.$mount()
