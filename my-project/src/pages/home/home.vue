ec9baf0506beb41d3a70d0cdb2e01651f584b883
杨世川 <yscbbc@gmail.com>
2019-07-30 16:20:53 +0800

    首页模版封装完成

--- my-project/src/pages/home/home.vue
+++ my-project/src/pages/home/home.vue
@@ -1,28 +1,47 @@
 <template>
-    <div class="home-swiper-class">
-        <swiper
-            :indicator-dots="isShowDots"
-            :autoplay="isAutoplay"
-            :interval="interval"
-            :duration="duration"
-            :circular="isCircular"
-            indicator-color="#FFFFFF"
-            indicator-active-color="#FFA439">
-            <swiper-item>
-                <img src="/static/images/carousel/1.jpg" alt="">
-            </swiper-item>
-            <swiper-item>
-                <img src="/static/images/carousel/2.jpg" alt="">                    
-            </swiper-item>
-            <swiper-item>
-                <img src="/static/images/carousel/3.jpg" alt="">
-            </swiper-item>
-        </swiper>
+    <div class="home">
+        <!-- ????? -->
+        <div class="home-swiper-class">
+            <swiper
+                :indicator-dots="isShowDots"
+                :autoplay="isAutoplay"
+                :interval="interval"
+                :duration="duration"
+                :circular="isCircular"
+                indicator-color="#FFFFFF"
+                indicator-active-color="#FFA439">
+                <swiper-item>
+                    <img @click="clickItem" src="/static/images/carousel/1.jpg" alt="">
+                </swiper-item>
+                <swiper-item>
+                    <img src="/static/images/carousel/2.jpg" alt="">                    
+                </swiper-item>
+                <swiper-item>
+                    <img src="/static/images/carousel/3.jpg" alt="">
+                </swiper-item>
+            </swiper>
+        </div>
+        <!-- ??? -->
+        <div class="line"></div>
+        <!-- ??????????????? -->
+        <div class="warn-list">
+            <home-cell></home-cell>
+        </div>
+        <!-- ??? -->
+        <div class="line"></div>
+        <!-- ?????? + ?? -->
+        <div class="convenient-list">
+
+        </div>
     </div>
 </template>
 
 <script>
+    import homeCell from './template/homeCell'
     export default{
+        components:{
+            homeCell
+        },
         data() {
             return {
                 isShowDots: true,
@@ -31,7 +50,12 @@
                 duration: 500,
                 isCircular: true
             }
-        }
+        },
+        methods: {
+            clickItem(){
+                console.log('??item');
+            }
+        },
     }
 </script>
 
@@ -41,10 +65,23 @@
     }
     .home-swiper-class swiper{
         width: 100%;
-        height: 150px;
+        height: 252rpx;
     } 
     .home-swiper-class swiper img{
         width: 100%;
         height: 100%;
+    }
+    .line{
+        background-color: #F1F2F4;
+        height: 20rpx;
+        width: 100%;
+    }
+    .warn-list{
+        background-color: white;
+    }
+    .warn-list 
+
+    .convenient-list{
+        background-color: antiquewhite;
     }
 </style>
---
+++ my-project/src/pages/home/template/homeCell.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="container">
+        <!-- 图片 -->
+        <img src="/static/images/home/validity_warn.png" alt="">
+        <!-- 文字 -->
+        <div class="title-container">
+            <div>有效期预警</div>
+            <div>共12条预警信息</div>
+        </div>
+        <!-- 数字(这里要添加一个padding类，保证圆角好用) -->
+        <div class="count">1</div>
+        <!-- 箭头 -->
+        <img src="/static/images/home/moreButton.png" alt="">
+    </div>
+</template>
+
+<script>
+    export default {
+        props:[
+            'imageUrl',
+            'title',
+            'subTitle',
+            'count'
+        ],
+        data () {
+            return {
+            };
+        }
+    }
+</script>
+
+<style>
+    .container{
+        border: 1rpx solid blue;
+        display: flex;
+        align-items: center;
+    }
+    .container > img:first-child{
+        width: 90rpx;
+        height: 90rpx;
+        margin: 20rpx;
+    }
+    .title-container{
+        flex-grow: 1;
+    }
+    .title-container > div:first-child{
+        font-size: 32rpx;
+        font-weight: 500;
+        color: #333333;
+    }
+    .title-container > div:last-child{
+        font-size: 28rpx;
+        font-weight: 400;
+        color: #999999;
+    }
+    .count{
+        background-color: #F43530;
+        color: white;
+        height: 36rpx;
+        min-width: 36rpx;        
+        line-height: 36rpx;
+        text-align: center;
+        margin: 0rpx 16rpx 0rpx 16rpx;
+        border-radius: 18rpx;
+    }
+    .container > img:last-child{
+        width: 16rpx;
+        height: 27rpx;
+        margin-right: 24rpx;
+    }
+
+</style>
