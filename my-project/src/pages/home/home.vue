478dfa4a43aecc2ee8df389e473e730bc06b8ddf
杨世川 <yscbbc@gmail.com>
2019-07-30 11:19:26 +0800

    轮播图界面

--- my-project/src/pages/home/home.vue
+++ my-project/src/pages/home/home.vue
@@ -1,6 +1,16 @@
 <template>
-    <div>
-        <h3>这是首页</h3>
+    <div class="home-swiper-class">
+        <swiper>
+            <swiper-item>
+                <img src="/static/images/carousel/1.jpg" alt="">
+            </swiper-item>
+            <swiper-item>
+                <img src="/static/images/carousel/2.jpg" alt="">                    
+            </swiper-item>
+            <swiper-item>
+                <img src="/static/images/carousel/3.jpg" alt="">
+            </swiper-item>
+        </swiper>
     </div>
 </template>
 
@@ -8,7 +18,15 @@
 </script>
 
 <style>
-    div{
-        background-color: red;
+    .home-swiper-class{
+        background-color: rgb(240, 16, 65)
+    }
+    .home-swiper-class swiper{
+        width: 100%;
+        height: 150px;
+    } 
+    .home-swiper-class swiper img{
+        width: 100%;
+        height: 100%;
     }
 </style>
