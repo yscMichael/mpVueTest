a9f1af06a3fcff9a24be055f3898d2088a069a7b
杨世川 <yscbbc@gmail.com>
2019-08-06 11:06:38 +0800

    药品初始化顶部栏

--- my-project/src/pages/drugStore/drugInit/drugInit.vue
+++ my-project/src/pages/drugStore/drugInit/drugInit.vue
@@ -1,6 +1,13 @@
 <template>
-    <div>
-        <h3>我是药品初始化</h3>
+    <div class="mainBackgroundView">
+        <!-- 顶部栏 -->
+        <div class="toolBar">
+            <div :class="['toolBarItem',(selectIndex == i)?'toolBarActive':'']" 
+                 v-for="(item,i) in buttonArray" 
+                 :key="i">
+              {{item.title}}
+            </div>
+        </div>
     </div>
 </template>
 
@@ -8,11 +15,42 @@
 export default {
   data () {
     return {
+      selectIndex:0,
+      buttonArray:[
+        {id:0, title:'西药'},
+        {id:1, title:'中成药'},
+        {id:0, title:'中药'},
+        {id:0, title:'医疗器械'}
+      ]
     };
   }
 }
 </script>
 
 <style>
-
+  .mainBackgroundView{
+    width: 100%;
+    height: 100%;
+    background-color:#f0f0f0;
+  }
+  .toolBar{
+    position:fixed;
+    top:0px;
+    width:100%;
+    height:40px;
+    font-size:15px;
+    z-index:9999;
+    display:flex;
+    background-color:white
+  }
+  .toolBarItem{
+    flex-grow:1;
+    height:40px;
+    line-height:40px;
+    text-align:center;
+  }
+  .toolBarActive{
+      border-bottom: 2px solid #1F91F2;
+      color: #1F91F2;
+  }
 </style>
