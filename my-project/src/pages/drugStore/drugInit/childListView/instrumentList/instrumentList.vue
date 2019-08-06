acf71f0a3c0393888142deba0a184d7ab307fd58
杨世川 <yscbbc@gmail.com>
2019-08-06 17:01:51 +0800

    药品初始化列表界面完成

--- my-project/src/pages/drugStore/drugInit/childListView/instrumentList/instrumentList.vue
+++ my-project/src/pages/drugStore/drugInit/childListView/instrumentList/instrumentList.vue
@@ -1,18 +1,38 @@
 <template>
-    <div>
-        <h3>医疗器械列表</h3>
-    </div>
+    <scroll-view class="west-main"
+                 scroll-y="true">
+        <initdrug-list-cell @clickCell="clickDrugListCell" 
+                            v-for="(item,index) in dataSource" 
+                            :key="index" 
+                            :item="item">
+        </initdrug-list-cell>
+    </scroll-view>
 </template>
 
 <script>
+import initdrugListCell from '@/pages/drugStore/drugInit/childListView/cellTemplate/initdrugListCell'
 export default {
+  components:{
+    initdrugListCell
+  },
   data () {
     return {
+      dataSource:[1,2,3,4,5,6,7,8,9,10,11,12,13,14]
     };
-  }
+  },
+  methods: {
+    clickDrugListCell(){
+      console.log('点击cell');
+    }
+  },
 }
 </script>
 
 <style scoped>
-
+  .west-main{
+      background-color: #eee;
+      width: 100%;
+      height: 100%;
+      box-sizing: border-box;
+  }
 </style>
