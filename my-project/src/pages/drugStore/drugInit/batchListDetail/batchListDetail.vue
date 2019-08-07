4de1eddf88e657fa7d31f87b4ce7432ba36e62e5
杨世川 <yscbbc@gmail.com>
2019-08-07 09:38:23 +0800

    批次详情界面搭建

---
+++ my-project/src/pages/drugStore/drugInit/batchListDetail/batchListDetail.vue
@@ -0,0 +1,20 @@
+<template>
+    <batch-list-cell></batch-list-cell>
+</template>
+
+<script>
+import batchListCell from '/src/pages/drugStore/drugInit/batchListDetail/template/batchListCell'
+export default {
+  components:{
+    batchListCell
+  },  
+  data () {
+    return {
+    };
+  }
+}
+</script>
+
+<style>
+
+</style>
