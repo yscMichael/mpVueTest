e991bff44044d683699fd361659c614d2523944c
杨世川 <yscbbc@gmail.com>
2019-08-07 11:03:14 +0800

    库存流水界面搭建

---
+++ my-project/src/pages/drugStore/drugInit/inventoryFlow/inventoryFlow.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <flow-cell v-for="(item,index) in dataSource" :key="index"></flow-cell>
+    </div>
+</template>
+
+<script>
+import flowCell from '@/pages/drugStore/drugInit/inventoryFlow/template/flowCell'
+export default {
+  components:{
+      flowCell
+  },  
+  data () {
+    return {
+      dataSource:[1,2,3,4,5,6,7,8,9,0]
+    };
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- my-project/src/pages/drugStore/drugInit/inventoryFlow/template/flowCell.vue
+++ my-project/src/pages/drugStore/drugInit/inventoryFlow/template/flowCell.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="main-view">
-        <div class="line-view"></div>
+        <div class="head-view">
+            <img src="/static/images/drugstore/drugInit/time.png " alt="">
+            <div>2018-12-28</div>
+        </div>
         <div class="content margin-top-10">
             <div>仓库：</div>
             <div>默认仓库</div>
@@ -14,23 +17,27 @@
             <div>CG-897890000012</div>
         </div>
         <div class="content margin-top-5">
-            <div>入库日期：</div>
+            <div>失效日期：</div>
             <div>2018-12-24</div>
         </div>
         <div class="content margin-top-5">
-            <div>失效日期：</div>
+            <div>批号：</div>
             <div>2019-12-24</div>
         </div>
         <div class="content margin-top-5">
-            <div>批号：</div>
+            <div>进货单价：</div>
             <div>100000</div>
         </div>
         <div class="content margin-top-5">
-            <div>进货单价：</div>
+            <div>入库量：</div>
             <div>12元</div>
         </div>
+        <div class="content margin-top-5 text-bold">
+            <div>剩余量：</div>
+            <div>0</div>
+        </div>
         <div class="content margin-top-5 margin-bottom-10">
-            <div>数量：</div>
+            <div>操作人：</div>
             <div>12.4盒</div>
         </div>
     </div>
@@ -48,11 +55,24 @@ export default {
 <style scoped>
     .main-view{
         background-color: white;
-        border: 1rpx solid white;
+        padding: 1rpx;
+        /* border: 1px solid red; */
     }
-    .line-view{
-        background-color: #F1F0F5;
-        height: 10px;
+    .head-view{
+        background-color: #F1F2F4;
+        height: 26px;
+        display: flex;
+        align-items: center;
+    }
+    .head-view > img{
+        width: 12px;
+        height: 12px;
+        margin-left: 12px;
+    }
+    .head-view > div{
+        margin-left: 5px;
+        color: #888888;
+        font-size: 12px;
     }
     .content{
         display: flex;
@@ -79,5 +99,8 @@ export default {
     }
     .margin-bottom-10{
         margin-bottom: 10px;
+    }
+    .text-bold{
+        font-weight: bold;
     }
 </style>
